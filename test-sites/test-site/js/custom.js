9c3f0faf856381555397a9b3ab63cc6a5fda1abc
098v <omarsin@gmx.com>
2018-06-27 02:06:49 -0400

    New update

--- test-sites/test-site/js/custom.js
+++
@@ -1,6 +0,0 @@
-// Navbar Colour Change
-window.onscroll = () => {
-	const header = document.querySelector('.mdl-layout__header');
-	if (this.scrollY <= 10) nav.className = '';
-	else header.className = 'scroll';
-};
